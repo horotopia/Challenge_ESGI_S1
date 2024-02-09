59c8ad2d86a282aad96370a3dd672c4655d3b6d4
Ilyes Kabrine <ilyeskabrine0@gmail.com>
2024-02-09 23:55:06 +0100

    FIX/prod&cat after refactor

---
+++ migrations/Version20240208094849.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240208094849 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE client_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE company_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE invoice_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE quote_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE quote_product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE category (id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE client (id INT NOT NULL, company_id_id INT DEFAULT NULL, last_name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, postal_code INT NOT NULL, phone VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, user_created VARCHAR(255) DEFAULT NULL, user_updated VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_C744045538B53C32 ON client (company_id_id)');
+        $this->addSql('CREATE TABLE company (id INT NOT NULL, name VARCHAR(255) NOT NULL, siret VARCHAR(14) NOT NULL, email VARCHAR(255) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, website VARCHAR(255) DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, address VARCHAR(255) DEFAULT NULL, postal_code INT DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, bank_account VARCHAR(255) DEFAULT NULL, logo VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN company.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE invoice (id INT NOT NULL, client_id_id INT DEFAULT NULL, quote_id_id INT DEFAULT NULL, status VARCHAR(255) NOT NULL, amount NUMERIC(10, 4) NOT NULL, payment_method VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, due_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, payment_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, user_validate VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_90651744DC2902E0 ON invoice (client_id_id)');
+        $this->addSql('CREATE INDEX IDX_9065174472BB1336 ON invoice (quote_id_id)');
+        $this->addSql('COMMENT ON COLUMN invoice.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE product (id INT NOT NULL, category_id_id INT DEFAULT NULL, company_id_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, brand VARCHAR(255) NOT NULL, unit_price NUMERIC(10, 4) NOT NULL, vat INT NOT NULL, available_quantity INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, user_created VARCHAR(255) NOT NULL, user_updated VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D34A04AD9777D11E ON product (category_id_id)');
+        $this->addSql('CREATE INDEX IDX_D34A04AD38B53C32 ON product (company_id_id)');
+        $this->addSql('CREATE TABLE quote (id INT NOT NULL, client_id_id INT DEFAULT NULL, quotation_number VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, total_ht DOUBLE PRECISION NOT NULL, total_ttc DOUBLE PRECISION NOT NULL, installments INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, due_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, user_created VARCHAR(255) NOT NULL, user_updated VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_6B71CBF4DC2902E0 ON quote (client_id_id)');
+        $this->addSql('CREATE TABLE quote_product (id INT NOT NULL, quote_id INT DEFAULT NULL, product_id INT DEFAULT NULL, quantity INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_3CA5AC29DB805178 ON quote_product (quote_id)');
+        $this->addSql('CREATE INDEX IDX_3CA5AC294584665A ON quote_product (product_id)');
+        $this->addSql('CREATE TABLE users (id INT NOT NULL, company_id_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, phone VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, token_registration VARCHAR(255) DEFAULT NULL, token_registration_life_time TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+        $this->addSql('CREATE INDEX IDX_1483A5E938B53C32 ON users (company_id_id)');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C744045538B53C32 FOREIGN KEY (company_id_id) REFERENCES company (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_90651744DC2902E0 FOREIGN KEY (client_id_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_9065174472BB1336 FOREIGN KEY (quote_id_id) REFERENCES quote (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD9777D11E FOREIGN KEY (category_id_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD38B53C32 FOREIGN KEY (company_id_id) REFERENCES company (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote ADD CONSTRAINT FK_6B71CBF4DC2902E0 FOREIGN KEY (client_id_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote_product ADD CONSTRAINT FK_3CA5AC29DB805178 FOREIGN KEY (quote_id) REFERENCES quote (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote_product ADD CONSTRAINT FK_3CA5AC294584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E938B53C32 FOREIGN KEY (company_id_id) REFERENCES company (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE category_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE client_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE company_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE invoice_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE product_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE quote_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE quote_product_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
+        $this->addSql('ALTER TABLE client DROP CONSTRAINT FK_C744045538B53C32');
+        $this->addSql('ALTER TABLE invoice DROP CONSTRAINT FK_90651744DC2902E0');
+        $this->addSql('ALTER TABLE invoice DROP CONSTRAINT FK_9065174472BB1336');
+        $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD9777D11E');
+        $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD38B53C32');
+        $this->addSql('ALTER TABLE quote DROP CONSTRAINT FK_6B71CBF4DC2902E0');
+        $this->addSql('ALTER TABLE quote_product DROP CONSTRAINT FK_3CA5AC29DB805178');
+        $this->addSql('ALTER TABLE quote_product DROP CONSTRAINT FK_3CA5AC294584665A');
+        $this->addSql('ALTER TABLE users DROP CONSTRAINT FK_1483A5E938B53C32');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE company');
+        $this->addSql('DROP TABLE invoice');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE quote');
+        $this->addSql('DROP TABLE quote_product');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Form\User\SearchType;
+use App\Model\SearchData;
 use App\Repository\CategoryRepository;
 use App\Form\CategoryType;
 use App\Form\ProductType;
@@ -23,32 +25,34 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/admin/product-category-management', name: 'app_categorie')]
+    #[Route('/admin/product-category-management', name: 'product_category_management')]
     public function addCategory(Request $request, EntityManagerInterface $entityManager, TokenGeneratorInterface $tokenGenerator , CategoryRepository $categoryRepository): Response
     {
         $category= new Category();
         $form=$this->createForm(CategoryType::class,$category);
 
+        $searchData = new SearchData();
+        $formSearchCategory = $this->createForm(SearchType::class, $searchData);
+
+        $formSearchCategory->handleRequest($request);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             //generate token
             $tokenRegistration= $tokenGenerator->generateToken();
-            
             $category->setName($form->get('name')->getData());
             $category->setDescription($form->get('description')->getData());
             $entityManager->persist($category);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_category');
-
+            return $this->redirectToRoute('product_category_management');
         }
         // $categories = $entityManager->getRepository(Category::class)->findAll();
         $categories = $entityManager->getRepository(Category::class)->getCategoriesWithProductCount($request->query->getInt('page', 1));
             // $categories=$catRepo->getCategoriesWithProductCount();
  
-        return $this->render('back/product_category_management/companies.html.twig', [
+        return $this->render('back/product_category_management/index.html.twig', [
             'formCategory' => $form->createView(),
             'categories' => $categories,
+            'formSearchCategory' => $formSearchCategory,
         ]);
     }
 
